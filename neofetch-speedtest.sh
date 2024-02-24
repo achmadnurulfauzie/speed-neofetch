565c42779f0dd1f4d1fa68043a2b7a123b3dadb6
Achmad Nurul Fauzie <113338492+achmadnurulfauzie@users.noreply.github.com>
2024-02-24 16:00:07 +0700

    Create neofetch-speedtest.sh

---
+++ neofetch-speedtest.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt install neofetch -y
+sudo apt-get install curl -y
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+sudo apt-get install speedtest -y
+neofetch
+speedtest
